f66396bbc52446138f16ae019a31604fbe5e59df
YannAGB <teambronew04@gmail.com>
2024-01-20 17:54:43 +0100

    creation et connection to database

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const dataBase = mysql.createConnection({
+  user: 'root',
+  password: '',
+  database: 'hotel',
+  host: 'localhost',
+});
+
+dataBase.connect((Error) => {
+  if (Error) throw Error;
+  console.log('connect to database successfully');
+});
+
+app.use((req, res) => {
+  res.status(201).json('hehoo');
+});
+
+app.listen(5000, () => {
+  console.log('Welcome to my server');
+});
